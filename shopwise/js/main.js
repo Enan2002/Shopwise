236adc44ec36132827046a7ee24322fae2b27685
Enan Amjad <107117753+Enan2002@users.noreply.github.com>
2023-03-31 00:57:19 +0500

    Add files via upload

---
+++ shopwise/js/main.js
@@ -0,0 +1,147 @@
+$(function () {
+  var siteSticky = function () {
+    $(".js-sticky-header").sticky({ topSpacing: 0 });
+  };
+  siteSticky();
+
+  var siteMenuClone = function () {
+    $(".js-clone-nav").each(function () {
+      var $this = $(this);
+      $this
+        .clone()
+        .attr("class", "site-nav-wrap")
+        .appendTo(".site-mobile-menu-body");
+    });
+
+    setTimeout(function () {
+      var counter = 0;
+      $(".site-mobile-menu .has-children").each(function () {
+        var $this = $(this);
+
+        $this.prepend('<span class="arrow-collapse collapsed">');
+
+        $this.find(".arrow-collapse").attr({
+          "data-toggle": "collapse",
+          "data-target": "#collapseItem" + counter,
+        });
+
+        $this.find("> ul").attr({
+          class: "collapse",
+          id: "collapseItem" + counter,
+        });
+
+        counter++;
+      });
+    }, 1000);
+
+    $("body").on("click", ".arrow-collapse", function (e) {
+      var $this = $(this);
+      if ($this.closest("li").find(".collapse").hasClass("show")) {
+        $this.removeClass("active");
+      } else {
+        $this.addClass("active");
+      }
+      e.preventDefault();
+    });
+
+    $(window).resize(function () {
+      var $this = $(this),
+        w = $this.width();
+
+      if (w > 768) {
+        if ($("body").hasClass("offcanvas-menu")) {
+          $("body").removeClass("offcanvas-menu");
+        }
+      }
+    });
+
+    $("body").on("click", ".js-menu-toggle", function (e) {
+      var $this = $(this);
+      e.preventDefault();
+
+      if ($("body").hasClass("offcanvas-menu")) {
+        $("body").removeClass("offcanvas-menu");
+        $this.removeClass("active");
+      } else {
+        $("body").addClass("offcanvas-menu");
+        $this.addClass("active");
+      }
+    });
+
+    // click outisde offcanvas
+    $(document).mouseup(function (e) {
+      var container = $(".site-mobile-menu");
+      if (!container.is(e.target) && container.has(e.target).length === 0) {
+        if ($("body").hasClass("offcanvas-menu")) {
+          $("body").removeClass("offcanvas-menu");
+        }
+      }
+    });
+  };
+  siteMenuClone();
+});
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 1,
+  grabCursor: true,
+  loop: true,
+  autoplay: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+$(document).ready(function () {
+  $(".customer-logos").slick({
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    arrows: false,
+    dots: false,
+    pauseOnHover: false,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 4,
+        },
+      },
+      {
+        breakpoint: 520,
+        settings: {
+          slidesToShow: 3,
+        },
+      },
+    ],
+  });
+});
+
+jQuery(document).ready(function ($) {
+  "use strict";
+  //  TESTIMONIALS CAROUSEL HOOK
+  $("#customers-testimonials").owlCarousel({
+    loop: true,
+    center: true,
+    items: 3,
+    margin: 0,
+    autoplay: true,
+    dots: true,
+    autoplayTimeout: 8500,
+    smartSpeed: 450,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      1170: {
+        items: 3,
+      },
+    },
+  });
+});
